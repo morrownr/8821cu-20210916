22f2cb7aa5898dcda6d273fb7b45564238d0e995
morrownr <morrownr@gmail.com>
2023-02-12 17:36:39 -0600

    fix some editing errors

--- install-driver.sh
+++ install-driver.sh
@@ -6,11 +6,15 @@
 #
 # To make this file executable:
 #
-# $ chmod +x edit-options.sh
+# $ chmod +x install-driver.sh
 #
 # To execute this file:
 #
-# $ sudo ./edit-options.sh
+# $ sudo ./install-driver.sh
+#
+# or
+#
+# $  sudo sh install-driver.sh
 #
 # Copyright(c) 2023 Nick Morrow
 #
